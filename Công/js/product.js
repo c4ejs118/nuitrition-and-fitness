b867b4bbe072c1b4d3a1db5d8ef8325052ed0fd8
phamduccong1234 <phamduccong1234@gmail.com>
2021-08-22 18:09:25 +0700

    first commit

---
+++ Công/js/product.js
@@ -0,0 +1,9 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+/* Hiển thị giá trị mặc định 50 cho thẻ span với id là demo*/
+output.innerHTML = slider.value; 
+
+/* cập nhật giá trị hiện tại của ranger slider*/
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
